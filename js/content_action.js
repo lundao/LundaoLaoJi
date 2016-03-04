2342e03eb64987bd62497e8c8f3fc9ac9ca42898
Nfer zhuang <nfer.zhuang@gmail.com>
2016-03-04 19:37:11 +0800

    support in https://www.lundao.com/people/ page

--- js/content_action.js
+++ js/content_action.js
@@ -14,12 +14,30 @@ var tipsHideTimer = null;
         var link= $(this);
         var offset = link.offset();
 
-        var data_id = $(this).attr('data-id');
-        if ( data_id == undefined )
-            return;
+        var data_id = undefined;
+        var displayName = undefined;
 
-        var displayName = $(this).text();
-        tips.html('<span style="padding:0 10px; color: #fff;">'+displayName+'是论道第'+data_id+'个注册的用户</span>');
+        // 在大部分页面
+        if ( link.attr('data-id') != undefined ) {
+            var data_id = link.attr('data-id');
+            var displayName = link.text();
+        }
+        // 在 https://www.lundao.com/people/ 页面
+        else if ( link.parent(".aw-item") != undefined ) {
+            var onclick = link.parent().parent().children(".operate").children(".follow").attr('onclick');
+            // AWS.User.follow($(this), 'user', 372);
+            var regExp = /, ([0-9]+)\);$/;
+            var matches = regExp.exec(onclick);
+            var data_id = matches[1];
+            var displayName = link.text();
+        }
+        else {
+            return ;
+        }
+
+        if ( displayName && data_id ) {
+            tips.html('<span style="padding:0 10px; color: #fff;">'+displayName+'是论道第'+data_id+'个注册的用户</span>');
+        }
 
         tips.show().offset({
             left: offset.left + link.width() + 5,
